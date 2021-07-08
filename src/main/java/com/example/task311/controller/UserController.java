e8b7523c0697cced1cbebfdb8385d28a85c7ca07
fradkinshtein <78609486+fradkinshtein@users.noreply.github.com>
2021-07-08 13:46:08 +0300

    Update UserController.java

--- src/main/java/com/example/task311/controller/UserController.java
+++ src/main/java/com/example/task311/controller/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.example.task311.service.RoleService;
 import com.example.task311.service.UserService;
 
 import java.security.Principal;
